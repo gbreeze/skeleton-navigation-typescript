17595c6c3e4a80ae6d7a8f65bdf1ca33f7f98626
Georg Perhofer <georg.perhofer@aimsoftware.com>
2015-12-22 14:54:58 +0100

    added "build-and-serve" task

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -4,7 +4,25 @@ var browserSync = require('browser-sync');
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
-gulp.task('serve', ['build'], function(done) {
+gulp.task('serve', function(done) {
+  browserSync({
+    online: false,
+    open: false,
+    port: 9000,
+    server: {
+      baseDir: ['.'],
+      middleware: function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    }
+  }, done);
+});
+
+// this task utilizes the browsersync plugin
+// to create a dev server instance
+// at http://localhost:9000
+gulp.task('build-and-serve', function(done) {
   browserSync({
     online: false,
     open: false,
